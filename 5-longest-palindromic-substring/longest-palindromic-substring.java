a649fb22e81522531b3b136c60c3b7438262605e
Suyasha Singh <134724770+suyashasingh@users.noreply.github.com>
2026-07-30 22:29:15 +0530

    Time: 14 ms (89.87%) | Memory: 43.9 MB (45.41%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,36 +1,46 @@
 class Solution {
-    public String longestPalindrome(String s) {
-        if (s == null || s.length() < 1) return "";
 
-        int start  = 0;
+    public String longestPalindrome(String s) {
+
+        if(s == null || s.length() < 1){
+            return "";
+        }
+
+        int start = 0;
         int end = 0;
 
-        for(int  i = 0 ;i < s.length(); i++){
-            int len1 = expand(s, i, i); //for odd length palindrome
-            int len2 = expand(s, i, i+1); //for even length palindrome
+        for(int i = 0; i < s.length(); i++){
+
+    
+            int len1 = expand(s, i, i);
+
+
+            int len2 = expand(s, i, i + 1);
 
             int len = Math.max(len1, len2);
-            if(len> end-start){
-                start = i - (len -1)/2;
-                end = i + len/2;
+
+ 
+            if(len > end - start){
+
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
             }
         }
 
-        return s.substring(start, end+1);
-        
+        return s.substring(start, end + 1);
     }
 
     public int expand(String s, int left, int right){
-        while(left>=0 && right<s.length() && s.charAt(left) == s.charAt(right)){
+
+        while(left >= 0 &&
+              right < s.length() &&
+              s.charAt(left) == s.charAt(right)){
+
             left--;
             right++;
         }
 
-        return right-left-1;
+     
+        return right - left - 1;
     }
 }
-
-
-//TC- O(n * n)
-//SC- O(1)
-//we have one more optimal that is called Manacher’s algorithm with O(n) but that is very complex
