003f702175b7afaf0e6d8e225b88b928eb2ddadc
Suyasha Singh <134724770+suyashasingh@users.noreply.github.com>
2026-08-16 13:04:55 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (78.59%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.java
+++ 88-merge-sorted-array/merge-sorted-array.java
@@ -1,31 +1,14 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
-        //Bruteforce approach
-
-        // int[] arr = new int[n+m];
-        // for(int i = 0; i<m; i++){
-        //     arr[i] = nums1[i];
-        // }
-        // for(int i = 0; i<n; i++){
-        //     arr[m+i] = nums2[i];
-        // }
-        // Arrays.sort(arr);
-        // for(int k = 0;k<arr.length;k++){
-        //     nums1[k] = arr[k];
-        // }
-
-        //without extra space
-
+        
         int i = m-1;
         int j = n-1;
-        int k = n+m-1;
+        int k = m+n-1;
 
         while(i>=0 && j>=0){
-            if(nums1[i]>nums2[j]){
-              nums1[k] = nums1[i];
-
-              i--;
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                i--;
             }
             else{
                 nums1[k] = nums2[j];
@@ -33,6 +16,7 @@ class Solution {
             }
             k--;
         }
+
         while(j>=0){
             nums1[k] = nums2[j];
             j--;
