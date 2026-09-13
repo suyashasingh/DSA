e901ef33e163ac8c0ab773869d34a5201ce84d77
Suyasha Singh <134724770+suyashasingh@users.noreply.github.com>
2026-09-13 13:10:04 +0530

    Time: 2 ms (99.16%) | Memory: 119.5 MB (45.44%) - LeetSync

--- 134-gas-station/gas-station.java
+++ 134-gas-station/gas-station.java
@@ -5,7 +5,8 @@ class Solution {
         int tank = 0;
         int start = 0;
         for(int i = 0; i<gas.length;i++){
-            totalgas+=gas[i]-cost[i];
+            totalgas+=gas[i]-cost[i]; // gas[i] -> how much gas is needed to travel from 1 point to another
+            //cost[i]-> how much cost it takes to travle 1 point to another
             tank+=gas[i]-cost[i];
 
             if(tank<0){
