f10d1fc0f427878216fac23162edcd66b49f4e92
Suyasha Singh <134724770+suyashasingh@users.noreply.github.com>
2026-09-13 13:51:20 +0530

    Time: 2 ms (100.00%) | Memory: 46.4 MB (61.45%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,32 +1,26 @@
 class Solution {
     public int romanToInt(String s) {
-
         int total = 0;
+        for(int i = 0; i<s.length(); i++){
+            int current = values(s.charAt(i));
 
-        for(int i = 0;i <s.length(); i++){
-            int current = value(s.charAt(i));
-
-            if(i<s.length()-1 && current<value(s.charAt(i+1))){
-                total -= current;
+            if(i<s.length()-1 && current<values(s.charAt(i+1))){
+                total-=current;
             }
             else{
-                total+=current; // when current is largere than the next or current is only the last element
+                total+=current;
             }
         }
         return total;
-        
     }
 
-    public int value(char c){
-         if (c == 'I') return 1;
-        if (c == 'V') return 5;
-        if (c == 'X') return 10;
-        if (c == 'L') return 50;
-        if (c == 'C') return 100;
-        if (c == 'D') return 500;
+    public int values(char ch){
+        if(ch == 'I') return 1;
+        if(ch == 'V') return 5;
+        if(ch == 'X') return 10;
+        if(ch == 'L') return 50;
+        if(ch == 'C') return 100;
+        if(ch == 'D') return 500;
         return 1000;
     }
 }
-
-//Tc = O(n)
-//SC = O(1)
